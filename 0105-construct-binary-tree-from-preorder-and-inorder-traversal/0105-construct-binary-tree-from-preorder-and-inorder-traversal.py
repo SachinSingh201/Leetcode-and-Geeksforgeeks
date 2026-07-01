02fadfe6c1c08e974b0f8b01f57131a15575bfff
Sachin Singh <sachin.singh239269@gmail.com>
2026-07-01 23:05:12 +0530

    Time: 142 ms (27.08%), Space: 16.8 MB (93.31%) - LeetHub

---
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -0,0 +1,58 @@
+# class Solution(object):
+#     def buildTree(self, preorder, inorder):
+
+#         inorder_map = {}
+#         for i, val in enumerate(inorder):
+#             inorder_map[val] = i
+
+#         preIdx = [0]
+
+#         def tree(left, right):
+
+#             if left > right:
+#                 return None
+
+#             root_val = preorder[preIdx[0]]
+#             preIdx[0] += 1
+
+#             root = TreeNode(root_val)
+
+#             mid = inorder_map[root_val]
+
+#             root.left = tree(left, mid - 1)
+#             root.right = tree(mid + 1, right)
+
+#             return root
+
+#         return tree(0, len(inorder) - 1)
+
+
+
+class Solution(object):
+    def buildTree(self, preorder, inorder):
+
+        preIdx = [0]
+
+        def search(inorder, val, left, right):
+            for i in range(left, right + 1):
+                if inorder[i] == val:
+                    return i
+
+        def tree(preorder, inorder, left, right):
+
+            if left > right:
+                return None
+
+            rootVal = preorder[preIdx[0]]
+            preIdx[0] += 1
+
+            node = TreeNode(rootVal)
+
+            mid = search(inorder, rootVal, left, right)
+
+            node.left = tree(preorder, inorder, left, mid - 1)
+            node.right = tree(preorder, inorder, mid + 1, right)
+
+            return node
+
+        return tree(preorder, inorder, 0, len(inorder) - 1)
