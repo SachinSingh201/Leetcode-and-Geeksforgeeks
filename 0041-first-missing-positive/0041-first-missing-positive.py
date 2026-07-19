352ae40bdddd5c92db4f98add69e53fa4f6b32f8
Sachin Singh <sachin.singh239269@gmail.com>
2026-07-19 19:04:39 +0530

    Time: 23 ms (93.94%), Space: 26.2 MB (8.61%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -4,20 +4,20 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        n = len(nums)
-        hashmap = {}
-        for i in range(1,n+2):
-            hashmap[i] = False
+        # n = len(nums)
+        # hashmap = {}
+        # for i in range(1,n+2):
+        #     hashmap[i] = False
         
-        for num in nums:
-            if num in hashmap:
-                hashmap[num] = True
-        ans = -1
-        for key , value in hashmap.items():
-            if value == False:
-                ans = key 
-                break 
-        return ans
+        # for num in nums:
+        #     if num in hashmap:
+        #         hashmap[num] = True
+        # ans = -1
+        # for key , value in hashmap.items():
+        #     if value == False:
+        #         ans = key 
+        #         break 
+        # return ans
 
 
 
@@ -33,5 +33,11 @@ class Solution(object):
         #     if nums[i]!= i+1:
         #         return i+1
         # return n+1
+
+        nums = set(nums)
+        i = 1
+        while i in nums:
+            i+=1
+        return i
 
         
