15c27ed2c1995d11f94553dd0d854a964e3e82d9
Sachin Singh <sachin.singh239269@gmail.com>
2026-07-20 23:10:10 +0530

    Time: 187 ms (13.68%), Space: 12.6 MB (80%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.py
+++ 1260-shift-2d-grid/1260-shift-2d-grid.py
@@ -1,22 +1,41 @@
+# class Solution(object):
+#     def shiftGrid(self, grid, k):
+#         n = len(grid)
+#         m = len(grid[0])
+
+#         arr = []
+
+#         for row in grid:
+#             arr.extend(row)
+
+#         k %= (n * m)
+
+#         arr = arr[-k:] + arr[:-k]
+
+#         ans = []
+
+#         idx = 0
+#         for i in range(n):
+#             ans.append(arr[idx:idx+m])
+#             idx += m
+
+#         return ans
+
 class Solution(object):
     def shiftGrid(self, grid, k):
         n = len(grid)
         m = len(grid[0])
 
-        arr = []
+        total = n * m
+        k %= total
 
-        for row in grid:
-            arr.extend(row)
+        while k:
+            curr = grid[n-1][m-1]
 
-        k %= (n * m)
+            for i in range(n):
+                for j in range(m):
+                    grid[i][j], curr = curr, grid[i][j]
 
-        arr = arr[-k:] + arr[:-k]
+            k -= 1
 
-        ans = []
-
-        idx = 0
-        for i in range(n):
-            ans.append(arr[idx:idx+m])
-            idx += m
-
-        return ans
+        return grid
