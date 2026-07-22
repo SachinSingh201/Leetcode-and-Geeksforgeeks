22fdb54dc21bd40c2aa8e0fff0f415d28c06eae8
Sachin Singh <sachin.singh239269@gmail.com>
2026-07-22 10:58:04 +0530

    Time: 133 ms (83.14%), Space: 57.8 MB (10.42%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def sortList(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        listA = []
+        curr = head
+        while curr:
+            listA.append([curr,curr.val])
+            curr = curr.next
+        listA.sort(key= lambda x:x[1] )    
+
+        dummy = ListNode(0)
+        dummy.next = None
+        tail = dummy 
+        for i in range(len(listA)):
+            dummy.next = listA[i][0]
+            dummy = dummy.next
+        dummy.next = None
+
+        return  tail.next
