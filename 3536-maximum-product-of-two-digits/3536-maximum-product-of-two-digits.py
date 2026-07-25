20dc54a5e0257c9f1a5e83ac29e62ddd8366992b
Sachin Singh <sachin.singh239269@gmail.com>
2026-07-26 01:03:18 +0530

    Time: 0 ms (100%), Space: 12.4 MB (59.44%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.py
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.py
@@ -1,18 +1,25 @@
+# class Solution(object):
+#     def maxProduct(self, n):
+#         """
+#         :type n: int
+#         :rtype: int
+#         """
+#         # num  = []
+#         # while n != 0:
+#         #     num.append(n%10)
+#         #     n= n// 10
+#         # if len(num)<2:
+#         #     return 0
+#         # x1 = max(num)
+#         # idx = num.index(x1)
+#         # num.pop(idx)
+#         # x2 = max(num)
+#         # return x1*x2
+
 class Solution(object):
     def maxProduct(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        num  = []
-        while n != 0:
-            num.append(n%10)
-            n= n// 10
-        if len(num)<2:
-            return 0
-        x1 = max(num)
-        idx = num.index(x1)
-        num.pop(idx)
-        x2 = max(num)
-        return x1*x2
+        m = sorted(str(n))
+        return int(m[-1])*int(m[-2])
+
+
         
