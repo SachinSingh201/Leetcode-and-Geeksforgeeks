75abb0c42d6083c347cb96a2c191255a9113fdb5
Sachin Singh <sachin.singh239269@gmail.com>
2026-07-28 16:04:47 +0530

    Time: 368 ms (70.59%), Space: 15.7 MB (11.76%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.py
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.py
@@ -5,13 +5,15 @@ class Solution(object):
         :rtype: str
         """
         partition = len(s)//2
-        bucket = [0]*26
+        # bucket = [0]*26
 
-        for i in range(partition):
-            bucket[ord(s[i]) - 97] += 1
-        left = "".join([chr(i+97) * bucket[i] for i in range(26)  if bucket[i] > 0])
+        # for i in range(partition):
+        #     bucket[ord(s[i]) - 97] += 1
+        # left = "".join([chr(i+97) * bucket[i] for i in range(26)  if bucket[i] > 0])
+        left =sorted(s[:partition])
+    
 
-        mid = s[partition] if len(s)%2 != 0 else ""
+        mid = [s[partition]] if len(s)%2 != 0 else []
         right = left[::-1]
-        return left+mid+right
+        return "".join(left+mid+right)
         
