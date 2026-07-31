fcc4f1c426d77ada29a45975e046dcd882bfdd1a
Sachin Singh <sachin.singh239269@gmail.com>
2026-07-31 18:03:31 +0530

    Time: 5 ms (97.1%), Space: 12.5 MB (20.76%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,24 +1,45 @@
 class Solution(object):
     def combinationSum(self, candidates, target):
+        candidates.sort()
+
         result = []
+        dp = set()         
 
         def dfs(index, target, path):
-            # Target achieve ho gaya
+
+           
             if target == 0:
                 result.append(path[:])
-                return
+                return True
 
-            # Invalid case
+           
             if target < 0 or index == len(candidates):
-                return
+                return False
 
-            # Choice 1: Current element lo
-            path.append(candidates[index])
-            dfs(index, target - candidates[index], path)
-            path.pop()
+            
+            if (index, target) in dp:
+                return False
 
-            # Choice 2: Current element skip karo
-            dfs(index + 1, target, path)
+            found = False
+
+            for i in range(index, len(candidates)):
+
+                # Since array is sorted
+                if candidates[i] > target:
+                    break
+
+                path.append(candidates[i])
+
+                if dfs(i, target - candidates[i], path):
+                    found = True
+
+                path.pop()
+
+           
+            if not found:
+                dp.add((index, target))
+
+            return found
 
         dfs(0, target, [])
         return result
