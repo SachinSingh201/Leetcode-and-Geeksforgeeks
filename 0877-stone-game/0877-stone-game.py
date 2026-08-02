b2730690205a8e0f622ed5f8d4c799ad89bf259c
Sachin Singh <sachin.singh239269@gmail.com>
2026-08-03 00:10:03 +0530

    Time: 705 ms (5.26%), Space: 47.9 MB (6.07%) - LeetHub

--- 0877-stone-game/0877-stone-game.py
+++ 0877-stone-game/0877-stone-game.py
@@ -1,7 +1,20 @@
 class Solution(object):
     def stoneGame(self, piles):
-        """
-        :type piles: List[int]
-        :rtype: bool
-        """
-        return True
+
+        memo = {}
+
+        def dp(i, j):
+            if i == j:
+                return piles[i]
+
+            if (i, j) in memo:
+                return memo[(i, j)]
+
+            memo[(i, j)] = max(
+                piles[i] - dp(i + 1, j),
+                piles[j] - dp(i, j - 1)
+            )
+
+            return memo[(i, j)]
+
+        return dp(0, len(piles) - 1) > 0
