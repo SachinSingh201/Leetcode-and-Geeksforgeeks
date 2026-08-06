7f436bc4730e410e68f49dcb677ab13a47299695
Sachin Singh <sachin.singh239269@gmail.com>
2026-08-06 09:51:27 +0530

    Time: 0 ms (100%), Space: 12.3 MB (98.03%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.py
@@ -0,0 +1,49 @@
+class Solution(object):
+    def floodFill(self, image, sr, sc, color):
+        """
+        :type image: List[List[int]]
+        :type sr: int
+        :type sc: int
+        :type color: int
+        :rtype: List[List[int]]
+        """
+
+        m = len(image)
+        n = len(image[0])
+
+        original = image[sr][sc]
+
+       
+        if original == color:
+            return image
+
+        stack = [(sr, sc)]
+        visited = set()
+        visited.add((sr, sc))
+
+        while stack:
+            i, j = stack.pop()
+
+            image[i][j] = color
+
+            directions = [
+                (1, 0),
+                (-1, 0),
+                (0, 1),
+                (0, -1)
+            ]
+
+            for dx, dy in directions:
+
+                x = i + dx
+                y = j + dy
+
+                if (0 <= x < m and
+                    0 <= y < n and
+                    (x, y) not in visited and
+                    image[x][y] == original):
+
+                    visited.add((x, y))
+                    stack.append((x, y))
+
+        return image
