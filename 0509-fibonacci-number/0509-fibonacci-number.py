3bed3facec94999664cad614371928131a3d764e
Sachin Singh <sachin.singh239269@gmail.com>
2026-08-23 22:24:05 +0530

    Time: 14 ms (78.34%), Space: 12.4 MB (54.95%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,6 +1,14 @@
 class Solution(object):
     def fib(self, n):
-       
-        if n == 0 or n ==1:
-            return  n
-        return  self.fib(n-1) + self.fib(n-2)
+        """
+        :type n: int
+        :rtype: int
+        """
+
+        memo = [0, 1]
+        for i in range(2, n+1):
+            memo.append(memo[i-1]+memo[i-2])
+
+        return memo[n]
+
+        
