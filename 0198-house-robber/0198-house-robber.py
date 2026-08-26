9c72ce2f5b6658ba91c04903b3d7c48cad9194b1
Sachin Singh <sachin.singh239269@gmail.com>
2026-08-26 09:32:24 +0530

    Time: 0 ms (100%), Space: 12.3 MB (90.79%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,20 +1,21 @@
 class Solution(object):
-    def rec(self,i,nums,dp):
-        if i >= len(nums):
-            return 0 
-        if dp[i] != -1:
-            return dp[i]
-        take = nums[i] + self.rec(i+2,nums,dp)
-
-        nonTake = self.rec(i+1,nums,dp)
-
-        dp[i] = max(take,nonTake)
-        return dp[i]
-
+   
     def rob(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        dp = [-1]*len(nums)
-        return self.rec(0,nums,dp)
+        n = len(nums)
+        if  n ==1:
+            return nums[0]
+        a = nums[0]
+        b = max(nums[0],nums[1])
+
+        for i in range(2,n):
+            take = a+nums[i]
+            not_take = b 
+            c = max(take,not_take)
+
+            a = b
+            b = c
+        return b
