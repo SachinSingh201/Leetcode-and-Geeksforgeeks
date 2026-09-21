aa2468cc4baf78452787d7e28e80198676578f17
Sachin Singh <sachin.singh239269@gmail.com>
2026-09-22 00:18:48 +0530

    Time: 562 ms (66.67%), Space: 21.4 MB (0%) - LeetHub

---
+++ 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def resultArray(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        
+        cnt = [0] * k
+        result = [0] * k
+        
+        for num in nums:
+            a = num % k
+            nxtcnt = [0] * k
+            
+           
+            for m in range(k):
+                mod = (m * a) % k
+                nxtcnt[mod] += cnt[m]
+                result[mod] += cnt[m]
+                
+            
+            nxtcnt[a] += 1
+            result[a] += 1
+            
+            
+            cnt = nxtcnt
+            
+        return result
